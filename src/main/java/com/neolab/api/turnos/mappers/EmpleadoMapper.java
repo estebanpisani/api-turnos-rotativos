36f521be4df86b1c20d332cf3980d9b8e447abee
Esteban Pisani <estebandpisani@gmail.com>
2022-09-28 17:01:52 -0300

    EmpleadoMapper added

---
+++ src/main/java/com/neolab/api/turnos/mappers/EmpleadoMapper.java
@@ -0,0 +1,60 @@
+package com.neolab.api.turnos.mappers;
+
+import com.neolab.api.turnos.dto.EmpleadoDTO;
+import com.neolab.api.turnos.entity.Empleado;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpleadoMapper {
+
+    public EmpleadoDTO entityToDTO(Empleado empleado){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        EmpleadoDTO dto = new EmpleadoDTO();
+
+        dto.setId(empleado.getId());
+        dto.setNombre(empleado.getNombre());
+        dto.setApellido(empleado.getApellido());
+        dto.setEmail(empleado.getEmail());
+        dto.setPassword(empleado.getPassword());
+        dto.setTelefono(empleado.getTelefono());
+        dto.setFechaDeNacimiento(empleado.getFechaDeNacimiento().format(formatter));
+        dto.setFechaAlta(empleado.getFechaAlta().format(formatter));
+
+        return dto;
+    }
+    public Empleado dtoToEntity(EmpleadoDTO dto){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        Empleado empleado = new Empleado();
+
+        empleado.setNombre(dto.getNombre());
+        empleado.setApellido(dto.getApellido());
+        empleado.setEmail(dto.getEmail());
+        empleado.setPassword(dto.getPassword());
+        empleado.setTelefono(dto.getTelefono());
+        LocalDate fechaDeNacimiento = LocalDate.parse(dto.getFechaDeNacimiento(), formatter );
+        LocalDate fechaDeAlta = LocalDate.parse(dto.getFechaAlta(), formatter );
+        empleado.setFechaDeNacimiento(fechaDeNacimiento);
+        empleado.setFechaAlta(fechaDeAlta);
+
+        return empleado;
+    }
+    public List<EmpleadoDTO> entityListToDTOList(List<Empleado> empleados){
+        List<EmpleadoDTO> dtos = new ArrayList<>();
+        for (Empleado empleado: empleados) {
+            EmpleadoDTO dto = this.entityToDTO(empleado);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+    public List<Empleado> dtoListToEntityList(List<EmpleadoDTO> dtos){
+        List<Empleado> empleados = new ArrayList<>();
+        for (EmpleadoDTO dto: dtos) {
+            Empleado empleado = this.dtoToEntity(dto);
+            empleados.add(empleado);
+        }
+        return empleados;
+    }
+}
--- src/main/java/com/neolab/api/turnos/service/impl/EmpleadoServiceImpl.java
+++ src/main/java/com/neolab/api/turnos/service/impl/EmpleadoServiceImpl.java
@@ -2,6 +2,7 @@ package com.neolab.api.turnos.service.impl;
 
 import com.neolab.api.turnos.dto.EmpleadoDTO;
 import com.neolab.api.turnos.entity.Empleado;
+import com.neolab.api.turnos.mappers.EmpleadoMapper;
 import com.neolab.api.turnos.repository.EmpleadoRepository;
 import com.neolab.api.turnos.service.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,34 +18,13 @@ import java.util.Optional;
 public class EmpleadoServiceImpl implements EmpleadoService {
     @Autowired
     EmpleadoRepository empleadoRepository;
+    @Autowired
+    EmpleadoMapper empleadoMapper;
 
     @Override
     public EmpleadoDTO createEmpleado(EmpleadoDTO dto) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        Empleado empleado = new Empleado();
-
-        empleado.setNombre(dto.getNombre());
-        empleado.setApellido(dto.getApellido());
-        empleado.setEmail(dto.getEmail());
-        empleado.setPassword(dto.getPassword());
-        empleado.setTelefono(dto.getTelefono());
-        LocalDate fechaDeNacimiento = LocalDate.parse(dto.getFechaDeNacimiento(), formatter );
-        LocalDate fechaDeAlta = LocalDate.parse(dto.getFechaAlta(), formatter );
-        empleado.setFechaDeNacimiento(fechaDeNacimiento);
-        empleado.setFechaAlta(fechaDeAlta);
-
-        Empleado newEmpleado = empleadoRepository.save(empleado);
-
-        EmpleadoDTO response = new EmpleadoDTO();
-        response.setId(newEmpleado.getId());
-        response.setNombre(newEmpleado.getNombre());
-        response.setApellido(newEmpleado.getApellido());
-        response.setEmail(newEmpleado.getEmail());
-        response.setPassword(newEmpleado.getPassword());
-        response.setTelefono(newEmpleado.getTelefono());
-        response.setFechaDeNacimiento(newEmpleado.getFechaDeNacimiento().format(formatter));
-        response.setFechaAlta(newEmpleado.getFechaAlta().format(formatter));
-
+        Empleado newEmpleado = empleadoRepository.save(empleadoMapper.dtoToEntity(dto));
+        EmpleadoDTO response = empleadoMapper.entityToDTO(newEmpleado);
         return response;
     }
 
@@ -56,45 +36,17 @@ public class EmpleadoServiceImpl implements EmpleadoService {
     @Override
     public List<EmpleadoDTO> getEmpleados() {
         List<Empleado> empleados = empleadoRepository.findAll();
-        List<EmpleadoDTO> dtos = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-
-        for (Empleado empleado: empleados) {
-            EmpleadoDTO dto = new EmpleadoDTO();
-            dto.setId(empleado.getId());
-            dto.setNombre(empleado.getNombre());
-            dto.setApellido(empleado.getApellido());
-            dto.setEmail(empleado.getEmail());
-            dto.setPassword(empleado.getPassword());
-            dto.setTelefono(empleado.getTelefono());
-            dto.setFechaDeNacimiento(empleado.getFechaDeNacimiento().format(formatter));
-            dto.setFechaAlta(empleado.getFechaAlta().format(formatter));
-            dtos.add(dto);
-        }
-
+        List<EmpleadoDTO> dtos = empleadoMapper.entityListToDTOList(empleados);
         return dtos;
     }
 
     @Override
     public EmpleadoDTO getEmpleadoById(Long id) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
         Optional<Empleado> opt = empleadoRepository.findById(id);
         if(opt.isPresent()){
-            EmpleadoDTO response = new EmpleadoDTO();
-            Empleado newEmpleado = opt.get();
-
-            response.setId(newEmpleado.getId());
-            response.setNombre(newEmpleado.getNombre());
-            response.setNombre(newEmpleado.getApellido());
-            response.setEmail(newEmpleado.getEmail());
-            response.setPassword(newEmpleado.getPassword());
-            response.setTelefono(newEmpleado.getTelefono());
-            response.setFechaDeNacimiento(newEmpleado.getFechaDeNacimiento().format(formatter));
-            response.setFechaAlta(newEmpleado.getFechaAlta().format(formatter));
-
+            EmpleadoDTO response = empleadoMapper.entityToDTO(opt.get());
             return response;
         }
-
         return null;
     }
 
