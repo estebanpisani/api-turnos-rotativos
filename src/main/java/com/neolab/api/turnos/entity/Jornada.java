999cc2a70690006b30b2e81785760ce21a4764f4
Esteban Pisani <estebandpisani@gmail.com>
2022-09-29 12:18:00 -0300

    Jornada con datetimeIn y datetimeOut

--- src/main/java/com/neolab/api/turnos/entity/Jornada.java
+++ src/main/java/com/neolab/api/turnos/entity/Jornada.java
@@ -1,4 +1,8 @@
 package com.neolab.api.turnos.entity;
 
+import java.time.LocalDateTime;
+
 public abstract class Jornada {
+    protected LocalDateTime timeIn;
+    protected LocalDateTime timeOut;
 }
