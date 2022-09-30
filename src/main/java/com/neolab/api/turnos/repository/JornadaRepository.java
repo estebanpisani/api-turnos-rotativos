2bdc110c9d7e8c8374b5da7b480c0f92222991ff
Esteban Pisani <estebandpisani@gmail.com>
2022-09-30 18:34:33 -0300

    Jornada Repository

---
+++ src/main/java/com/neolab/api/turnos/repository/JornadaRepository.java
@@ -0,0 +1,12 @@
+package com.neolab.api.turnos.repository;
+
+import com.neolab.api.turnos.entity.Jornada;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface JornadaRepository extends JpaRepository<Jornada, Long> {
+
+    Optional<Jornada> findByEmpleadoId(Long id);
+}
